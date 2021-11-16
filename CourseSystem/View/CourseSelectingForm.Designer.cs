b1b506469292f777b20abe8fd26a55215b094a76
1324 <1324.com>
2021-11-16 16:49:17 +0800

    UI test not done

--- CourseSystem/View/CourseSelectingForm.Designer.cs
+++ CourseSystem/View/CourseSelectingForm.Designer.cs
@@ -34,11 +34,12 @@ namespace CourseSystem
             this._viewOutcomeButton = new System.Windows.Forms.Button();
             this._courseTabControl = new System.Windows.Forms.TabControl();
             this._tabPage1 = new System.Windows.Forms.TabPage();
-            this._secondTabDataGridView = new System.Windows.Forms.DataGridView();
             this._tabPage2 = new System.Windows.Forms.TabPage();
+            this._secondTabDataGridView = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this._firstTabDataGridView)).BeginInit();
             this._courseTabControl.SuspendLayout();
             this._tabPage1.SuspendLayout();
+            this._tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this._secondTabDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,20 +49,22 @@ namespace CourseSystem
             this._firstTabDataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
             this._firstTabDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this._firstTabDataGridView.Location = new System.Drawing.Point(0, 0);
+            this._firstTabDataGridView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this._firstTabDataGridView.Name = "_firstTabDataGridView";
             this._firstTabDataGridView.RowHeadersVisible = false;
             this._firstTabDataGridView.RowHeadersWidth = 51;
             this._firstTabDataGridView.RowTemplate.Height = 27;
-            this._firstTabDataGridView.Size = new System.Drawing.Size(1408, 489);
+            this._firstTabDataGridView.Size = new System.Drawing.Size(1056, 391);
             this._firstTabDataGridView.TabIndex = 6;
             this._firstTabDataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ClickDataGridViewCellContent);
             // 
             // _submitConfirmButton
             // 
             this._submitConfirmButton.AutoSize = true;
-            this._submitConfirmButton.Location = new System.Drawing.Point(958, 537);
+            this._submitConfirmButton.Location = new System.Drawing.Point(718, 430);
+            this._submitConfirmButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this._submitConfirmButton.Name = "_submitConfirmButton";
-            this._submitConfirmButton.Size = new System.Drawing.Size(218, 70);
+            this._submitConfirmButton.Size = new System.Drawing.Size(164, 56);
             this._submitConfirmButton.TabIndex = 3;
             this._submitConfirmButton.Text = "確認送出";
             this._submitConfirmButton.UseVisualStyleBackColor = true;
@@ -70,9 +73,10 @@ namespace CourseSystem
             // _viewOutcomeButton
             // 
             this._viewOutcomeButton.AutoSize = true;
-            this._viewOutcomeButton.Location = new System.Drawing.Point(1193, 536);
+            this._viewOutcomeButton.Location = new System.Drawing.Point(895, 429);
+            this._viewOutcomeButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this._viewOutcomeButton.Name = "_viewOutcomeButton";
-            this._viewOutcomeButton.Size = new System.Drawing.Size(211, 71);
+            this._viewOutcomeButton.Size = new System.Drawing.Size(158, 57);
             this._viewOutcomeButton.TabIndex = 4;
             this._viewOutcomeButton.Text = "查看選課結果";
             this._viewOutcomeButton.UseVisualStyleBackColor = true;
@@ -82,23 +86,37 @@ namespace CourseSystem
             // 
             this._courseTabControl.Controls.Add(this._tabPage1);
             this._courseTabControl.Controls.Add(this._tabPage2);
-            this._courseTabControl.Location = new System.Drawing.Point(0, 6);
+            this._courseTabControl.Location = new System.Drawing.Point(0, 5);
+            this._courseTabControl.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this._courseTabControl.Name = "_courseTabControl";
             this._courseTabControl.SelectedIndex = 0;
-            this._courseTabControl.Size = new System.Drawing.Size(1414, 524);
+            this._courseTabControl.Size = new System.Drawing.Size(1060, 419);
             this._courseTabControl.TabIndex = 5;
             this._courseTabControl.SelectedIndexChanged += new System.EventHandler(this.ChangeTabIndex);
             // 
             // _tabPage1
             // 
             this._tabPage1.Controls.Add(this._firstTabDataGridView);
-            this._tabPage1.Location = new System.Drawing.Point(4, 25);
+            this._tabPage1.Location = new System.Drawing.Point(4, 22);
+            this._tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this._tabPage1.Name = "_tabPage1";
-            this._tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this._tabPage1.Size = new System.Drawing.Size(1450, 495);
+            this._tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this._tabPage1.Size = new System.Drawing.Size(1052, 393);
             this._tabPage1.TabIndex = 0;
             this._tabPage1.Text = "tabPage1";
             this._tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // _tabPage2
+            // 
+            this._tabPage2.Controls.Add(this._secondTabDataGridView);
+            this._tabPage2.Location = new System.Drawing.Point(4, 22);
+            this._tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this._tabPage2.Name = "_tabPage2";
+            this._tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this._tabPage2.Size = new System.Drawing.Size(1052, 393);
+            this._tabPage2.TabIndex = 1;
+            this._tabPage2.Text = "tabPage2";
+            this._tabPage2.UseVisualStyleBackColor = true;
             // 
             // _secondTabDataGridView
             // 
@@ -106,37 +124,29 @@ namespace CourseSystem
             this._secondTabDataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
             this._secondTabDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this._secondTabDataGridView.Location = new System.Drawing.Point(0, 0);
+            this._secondTabDataGridView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this._secondTabDataGridView.Name = "_secondTabDataGridView";
             this._secondTabDataGridView.RowHeadersVisible = false;
             this._secondTabDataGridView.RowHeadersWidth = 51;
             this._secondTabDataGridView.RowTemplate.Height = 27;
-            this._secondTabDataGridView.Size = new System.Drawing.Size(1459, 489);
+            this._secondTabDataGridView.Size = new System.Drawing.Size(1094, 391);
             this._secondTabDataGridView.TabIndex = 2;
-            // 
-            // _tabPage2
-            // 
-            this._tabPage2.Controls.Add(this._secondTabDataGridView);
-            this._tabPage2.Location = new System.Drawing.Point(4, 25);
-            this._tabPage2.Name = "_tabPage2";
-            this._tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this._tabPage2.Size = new System.Drawing.Size(1450, 495);
-            this._tabPage2.TabIndex = 1;
-            this._tabPage2.Text = "tabPage2";
-            this._tabPage2.UseVisualStyleBackColor = true;
             // 
             // CourseSelectingForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1421, 617);
+            this.ClientSize = new System.Drawing.Size(1066, 494);
             this.Controls.Add(this._courseTabControl);
             this.Controls.Add(this._viewOutcomeButton);
             this.Controls.Add(this._submitConfirmButton);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "CourseSelectingForm";
-            this.Text = "SelectCourseForm";
+            this.Text = "CourseSelectingForm";
             ((System.ComponentModel.ISupportInitialize)(this._firstTabDataGridView)).EndInit();
             this._courseTabControl.ResumeLayout(false);
             this._tabPage1.ResumeLayout(false);
+            this._tabPage2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this._secondTabDataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
---
+++ CourseSystemUITest/UITest.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseSystemUITest
+{
+    [TestClass()]
+    public class UITest
+    {
+        private Robot _robot;
+        private string targetAppPath;
+        //private const string START_UP_FORM = "StartUpForm";
+
+        private const string START_UP_FORM = "CourseSelectingForm";
+
+        // init
+        [TestInitialize]
+        public void Initialize()
+        {
+            var projectName = "CourseSystem";
+            string solutionPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\"));
+            targetAppPath = Path.Combine(solutionPath, projectName, "bin", "Debug", "CourseSystem.exe");
+            _robot = new Robot(targetAppPath, START_UP_FORM);
+        }
+
+        [TestCleanup()]
+        public void Cleanup()
+        {
+            _robot.CleanUp();
+        }
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            _robot.ClickButton("Course Selecting System");
+            _robot.AssertEnable("Course Selecting System", false);
+        }
+
+        [TestMethod]
+        public void TestButtonControl()
+        {
+            _robot.ClickButton("Course Selecting System");
+            _robot.AssertEnable("Course Selecting System", false);
+
+            _robot.SwitchTo("StartUpForm");
+            _robot.ClickButton("Course Management System");
+            _robot.AssertEnable("Course Management System", false);
+
+            _robot.SwitchTo("CourseSelectingForm");
+            _robot.CloseWindow();
+            _robot.AssertEnable("Course Selecting System", true);
+
+            _robot.SwitchTo("CourseManagementForm");
+            _robot.CloseWindow();
+            _robot.AssertEnable("Course Management System", true);
+        }
+    }
+
+}
