4cdef9779b6d20d9c4d86cd8af3cc97408650ee2
lohsuan <angelalo88362@gmail.com>
2021-11-23 08:48:26 +0800

    hw5: not done

--- CourseSystemUITest/UITest.cs
+++ CourseSystemUITest/UITest.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace CourseSystemUITest
 {
     /// <summary>
-    /// Summary description for MainFormUITest
+    /// Summary description for CourseSystemUITest
     /// </summary>
     [CodedUITest]
     public class UITest
@@ -19,6 +19,7 @@ namespace CourseSystemUITest
             Robot.Initialize(FILE_PATH, APP_TITLE);
         }
 
+        // StartUpForm
         [TestMethod]
         public void StartUpFormButtonControlTest()
         {
@@ -33,6 +34,45 @@ namespace CourseSystemUITest
 
             Robot.CloseWindow("CourseManagementForm");
             Robot.AssertButtonEnable("Course Management System", true);
+        }
+
+        [TestMethod]
+        public void CourseSelectingFormTest()
+        {
+            Robot.ClickButton("Course Selecting System");
+            Robot.SetForm("CourseSelectingForm");
+            
+            Robot.AssertButtonEnable("確認送出", false);
+            Robot.AssertButtonEnable("查看選課結果", true);
+
+            //Robot.SetCheckBox("選 資料列 9", true);
+
+            //Robot.AssertButtonEnable("確認送出", true);
+
+            Robot.AssertDataItemsInDataGridView("DataGridView", 12);
+
+            Robot.ClickTabControl("電子三甲");
+            Robot.SetCheckBox("選 資料列 3", true);
+            
+            Robot.ClickButton("查看選課結果");
+            Robot.AssertButtonEnable("查看選課結果", false);
+
+            Robot.ClickButton("確認送出");
+
+            Robot.AssertButtonEnable("確認送出", false);
+            //Robot.CloseWindow("");
+
+            //Robot.AssertText("加選成功\n");
+            //Robot.SendKeyEnterToMessageBox();
+
+            //Robot.CloseWindow("CourseSelectingForm");
+            //Robot.AssertButtonEnable("Course Selecting System", true);
+
+            //Robot.ClickButton("Course Management System");
+            //Robot.AssertOtherFormButtonEnable("StartUpForm", "Course Management System", false);
+
+            //Robot.CloseWindow("CourseManagementForm");
+            //Robot.AssertButtonEnable("Course Management System", true);
         }
 
         // Cleanup
